68dc7e869f4f113fb208bc9e6d5cb17ebdc9f47a
Chen,zhiliang <110036350+LEON-LUCKY@users.noreply.github.com>
2023-10-26 16:58:36 +0800

    Update qsort.js

--- utils/qsort.js
+++ utils/qsort.js
@@ -16,3 +16,46 @@ const qsort = arr => {
 	// 递归分割已分割数组
 	return [...qsort(left), pivot, ...qsort(right)];
 };
+// js 选择排序方法
+const sortMethods = {
+	// 升序
+	asc: function (prop) {
+		return function (obj1, obj2) {
+			var val1 = obj1[prop];
+			var val2 = obj2[prop];
+			if (!isNaN(Number(val1)) && !isNaN(Number(val2))) {
+				val1 = Number(val1);
+				val2 = Number(val2);
+			}
+			if (val1 < val2) {
+				return -1;
+			} else if (val1 > val2) {
+				return 1;
+			} else {
+				return 0;
+			}
+		};
+	},
+	// 降序
+	desc: function (prop) {
+		return function (obj1, obj2) {
+			var val1 = obj1[prop];
+			var val2 = obj2[prop];
+			if (!isNaN(Number(val1)) && !isNaN(Number(val2))) {
+				val1 = Number(val1);
+				val2 = Number(val2);
+			}
+			if (val1 < val2) {
+				return 1;
+			} else if (val1 > val2) {
+				return -1;
+			} else {
+				return 0;
+			}
+		};
+	}
+};
+// 应用选择排序方法
+const sortMethodsApply = (arr, prop, method) => {
+	return arr.sort(sortMethods[method](prop));
+};
